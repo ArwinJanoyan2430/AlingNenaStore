1be71ca25db376de66d4330aac5eec5a6353b1ed
Arwin Ryan C. Janoyan <arwintiktok24@gmail.com>
2026-06-29 13:45:32 +0800

    123

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ const Card = ({ title, value, icon: Icon }) => {
         <div className="card card-hover p-5 sm:p-6 relative overflow-hidden group flex items-center justify-between bg-white rounded-xl shadow-sm border border-slate-100">
         
         {/* left accent bar */}
-        <div className="absolute left-0 top-0 bottom-0 w-1 rounded-r-full bg-amber-700 group-hover:bg-indigo-500/70 transition-colors" />
+        <div className="absolute left-0 top-0 bottom-0 w-1 rounded-r-full bg-amber-900 group-hover:bg-orange-500 transition-colors" />
 
         {/* text */}
         <div>
@@ -15,7 +15,7 @@ const Card = ({ title, value, icon: Icon }) => {
 
         {/* icon */}
         {Icon && (
-            <Icon className="size-10 p-2.5 rounded-lg bg-amber-700 text-white group-hover:bg-indigo-50 group-hover:text-indigo-600 transition-colors duration-200" />
+            <Icon className="size-10 p-2.5 rounded-lg bg-orange-100 text-black group-hover:bg-orange-500 group-hover:text-orange transition-colors duration-200" />
         )}
         </div>
     </div>
