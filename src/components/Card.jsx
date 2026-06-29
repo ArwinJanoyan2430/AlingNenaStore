c9b51cd2762e1fc44917f49ede356efc2f6c3cf3
Arwin Ryan C. Janoyan <arwintiktok24@gmail.com>
2026-06-29 13:36:04 +0800

    123123

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,24 @@
 const Card = ({ title, value, icon: Icon }) => {
   return (
-    <div className="bg-gray-900 p-5 rounded-xl shadow-md border border-white/10 hover:scale-105 transition">
-      <div className="flex items-center justify-between">
+    <div>
+        <div className="card card-hover p-5 sm:p-6 relative overflow-hidden group flex items-center justify-between bg-white rounded-xl shadow-sm border border-slate-100">
+        
+        {/* left accent bar */}
+        <div className="absolute left-0 top-0 bottom-0 w-1 rounded-r-full bg-amber-700 group-hover:bg-indigo-500/70 transition-colors" />
+
+        {/* text */}
         <div>
-          <p className="text-gray-400 text-sm">{title}</p>
-          <h2 className="text-white text-2xl font-bold">{value}</h2>
+            <p className="text-sm font-medium text-slate-700">{title}</p>
+            <p className="text-2xl font-bold text-slate-900 mt-1">{value}</p>
         </div>
 
-        {Icon && <Icon className="text-gray-400" size={22} />}
-      </div>
+        {/* icon */}
+        {Icon && (
+            <Icon className="size-10 p-2.5 rounded-lg bg-amber-700 text-white group-hover:bg-indigo-50 group-hover:text-indigo-600 transition-colors duration-200" />
+        )}
+        </div>
     </div>
+    
   );
 };
 
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,11 +3,24 @@ import { DollarSign, ShoppingCart, Package, AlertTriangle } from "lucide-react";
 
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-4 gap-4 p-3 mt-10 ">
-      <Card title="Total Sales" value="₱ 12,450" icon={DollarSign} />
-      <Card title="Transactions" value="128" icon={ShoppingCart} />
-      <Card title="Products" value="56" icon={Package} />
-      <Card title="Low Stock" value="7" icon={AlertTriangle} />
+    <div className="p-6 mt-24 md:mt-0">
+      {/* Header */}
+      <div className="max-w-6xl mx-auto mb-8">
+        <h1 className="text-3xl font-bold text-slate-900">
+          Store Dashboard
+        </h1>
+        <p className="text-slate-600 mt-1">
+          Sales and transactions overview
+        </p>
+      </div>
+
+      {/* Cards */}
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 max-w-6xl mx-auto">
+        <Card title="Total Sales" value="₱ 12,450" icon={DollarSign} />
+        <Card title="Transactions" value="128" icon={ShoppingCart} />
+        <Card title="Products" value="56" icon={Package} />
+        <Card title="Low Stock" value="7" icon={AlertTriangle} />
+      </div>
     </div>
   );
 };
