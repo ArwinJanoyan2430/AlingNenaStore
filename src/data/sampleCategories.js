482e964174470a6659575493874cbbe162473b2a
Arwin Ryan C. Janoyan <arwintiktok24@gmail.com>
2026-07-04 11:34:44 +0800

    Initial commit

---
+++ src/data/sampleCategories.js
@@ -0,0 +1,94 @@
+// src/data/sampleCategories.js
+
+export const sampleCategories = [
+  {
+    id: 1,
+    name: "Drinks",
+    created_at: null,
+  },
+  {
+    id: 2,
+    name: "Frozen Foods",
+    created_at: null,
+  },
+  {
+    id: 3,
+    name: "Dairy",
+    created_at: null,
+  },
+  {
+    id: 4,
+    name: "Biscuits",
+    created_at: null,
+  },
+  {
+    id: 5,
+    name: "Cigarettes",
+    created_at: null,
+  },
+  {
+    id: 6,
+    name: "Rice & Grains",
+    created_at: null,
+  },
+  {
+    id: 7,
+    name: "Personal Care",
+    created_at: null,
+  },
+  {
+    id: 8,
+    name: "Instant Noodles",
+    created_at: null,
+  },
+  {
+    id: 9,
+    name: "Canned Goods",
+    created_at: null,
+  },
+  {
+    id: 10,
+    name: "Junk Foods",
+    created_at: null,
+  },
+  {
+    id: 11,
+    name: "Bread & Bakery",
+    created_at: null,
+  },
+  {
+    id: 12,
+    name: "Laundry & Cleaning",
+    created_at: null,
+  },
+  {
+    id: 13,
+    name: "Pet Supplies",
+    created_at: null,
+  },
+  {
+    id: 14,
+    name: "Condiments",
+    created_at: null,
+  },
+  {
+    id: 15,
+    name: "School Supplies",
+    created_at: null,
+  },
+  {
+    id: 16,
+    name: "Others",
+    created_at: null,
+  },
+  {
+    id: 17,
+    name: "Coffee & Powdered Drinks",
+    created_at: null,
+  },
+  {
+    id: 18,
+    name: "Candies",
+    created_at: null,
+  },
+];
